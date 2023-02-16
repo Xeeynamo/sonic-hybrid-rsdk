cdf828f2b4c2b14f95fbbea019328b2df9266dae
Crunch (Chaz9) <99685295+CCIGAMES@users.noreply.github.com>
2023-02-16 11:52:20 +0000

    Used ChatGPT again but im not sure it came out right
    It said: Here's an updated version of the script with support for both RSDKv3 and RSDKv4.

--- SonicHybridRsdk.Generator/RsdkGenericImporter.cs
+++ SonicHybridRsdk.Generator/RsdkGenericImporter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,88 +6,97 @@ using static SonicHybridRsdk.Generator.Global;
 
 namespace SonicHybridRsdk.Generator
 {
-    class RsdkGenericImporter
+class RsdkGenericImporter
+{
+public static void PatchStageConfig(Context context, Func<Stream, IStageConfig> stageConfigRead, string srcFile, string dstFile)
+{
+var src = OpenRead(srcFile, stageConfigRead);
+PatchGameObjects(src.Sfx, context.Replacements);
+PatchGameObjects(src.Objects, context.Replacements);
+if (context.Version == RsdkVersion.RSDKv3)
+{
+Create(dstFile, stream => new RSDKv3.StageConfig
+{
+Objects = src.Objects,
+Sfx = src.Sfx,
+UnknownData = src.UnknownData,
+}.Write(stream));
+}
+else if (context.Version == RsdkVersion.RSDKv4)
+{
+Create(dstFile, stream => new RSDKv4.StageConfig
+{
+Objects = src.Objects,
+Sfx = src.Sfx,
+UnknownData = src.UnknownData,
+}.Write(stream));
+}
+else
+{
+throw new ArgumentException("Invalid RSDK version specified in context.");
+}
+}
+      public static void PatchGameObjects(List<GameObject> gameObjects, Dictionary<string, string> replacements)
     {
-        public static void PatchStageConfig(Context context, Func<Stream, IStageConfig> stageConfigRead, string srcFile, string dstFile)
+        if ((replacements?.Count ?? 0) == 0)
+            return;
+
+        foreach (var item in gameObjects)
         {
-            var src = OpenRead(srcFile, stageConfigRead);
-            PatchGameObjects(src.Sfx, context.Replacements);
-            PatchGameObjects(src.Objects, context.Replacements);
-            Create(dstFile, stream => new StageConfig
-            {
-                Objects = src.Objects,
-                Sfx = src.Sfx,
-                UnknownData = src.UnknownData,
-            }.Write(stream));
+            if (replacements.TryGetValue(item.Name, out var newName))
+                item.Name = newName;
+            if (replacements.TryGetValue(item.Path, out var newPath))
+                item.Path = newPath;
         }
+    }
 
-        public static void PatchGameObjects(List<GameObject> gameObjects, Dictionary<string, string> replacements)
+    public static void PatchStage(
+        Context context,
+        Func<Stream, IStageAct> stageActRead,
+        string srcFile, string dstFile,
+        Func<Context, IEntity, string, bool> entityAction = null,
+        Action<Context, IStageAct> postAction = null)
+    {
+        var act = OpenRead(srcFile, stageActRead);
+        for (int i = 0; i < act.Entities.Count; i++)
         {
-            if ((replacements?.Count ?? 0) == 0)
-                return;
-
-            foreach (var item in gameObjects)
+            var entity = act.Entities[i];
+            var id = entity.Type;
+            if (id < context.SrcConfig.GameObjects.Count)
             {
-                if (replacements.TryGetValue(item.Name, out var newName))
-                    item.Name = newName;
-                if (replacements.TryGetValue(item.Path, out var newPath))
-                    item.Path = newPath;
+                if (id == 0)
+                    continue;
+                var srcObj = context.SrcObjects[id - 1].Name;
+                var dstObj = context.DstObjects[srcObj] + 1;
+                entity.Type = (byte)dstObj;
+                entityAction?.Invoke(context, entity, srcObj);
             }
-        }
-
-        public static void PatchStage(
-            Context context,
-            Func<Stream, IStageAct> stageActRead,
-            string srcFile, string dstFile,
-            Func<Context, IEntity, string, bool> entityAction = null,
-            Action<Context, IStageAct> postAction = null)
-        {
-            var act = OpenRead(srcFile, stageActRead);
-            for (int i = 0; i < act.Entities.Count; i++)
+            else
             {
-                var entity = act.Entities[i];
-                var id = entity.Type;
-                if (id < context.SrcConfig.GameObjects.Count)
+                if (entityAction == null ||
+                    id + 1 >= act.EntityNames.Count ||
+                    entityAction(context, entity, act.EntityNames[id - 1]) == false)
                 {
-                    if (id == 0)
-                        continue;
-                    var srcObj = context.SrcObjects[id - 1].Name;
-                    var dstObj = context.DstObjects[srcObj] + 1;
-                    entity.Type = (byte)dstObj;
-                    entityAction?.Invoke(context, entity, srcObj);
-                }
-                else
-                {
-                    if (entityAction == null ||
-                        id + 1 >= act.EntityNames.Count ||
-                        entityAction(context, entity, act.EntityNames[id - 1]) == false)
-                    {
-                        var localId = id - context.SrcConfig.GameObjects.Count;
-                        entity.Type = Convert.ToByte(context.DstConfig.GameObjects.Count + localId);
-                    }
+                    var localId = id - context.SrcConfig.GameObjects.Count;
+                    entity.Type = Convert.ToByte(context.DstConfig.GameObjects.Count + localId);
                 }
             }
+        }
 
-            postAction?.Invoke(context, act);
-            Create(dstFile, stream => new StageAct
+        postAction?.Invoke(context, act);
+        if (context.Version == RsdkVersion.RSDKv3)
+        {
+            Create(dstFile, stream => new RSDKv3.StageAct
             {
                 Title = act.Title,
                 Layers = act.Layers,
                 Width = act.Width,
                 Height = act.Height,
                 Layout = act.Layout,
-                Entities = act.Entities.Select(x => new Entity
+                Entities = act.Entities.Select(x => new RSDKv3.Entity
                 {
                     Type = x.Type,
                     PropertyValue = x.PropertyValue,
                     X = x.X,
-                    Y = x.Y,
-                    SubX = x.SubX,
-                    SubY = x.SubY,
-                    AttributeFlags = x.AttributeFlags,
-                    Attributes = x.Attributes,
-                }).Cast<IEntity>().ToList(),
-            }.Write(stream));
-        }
-    }
-}
+                    Y = x.Y
+ 
