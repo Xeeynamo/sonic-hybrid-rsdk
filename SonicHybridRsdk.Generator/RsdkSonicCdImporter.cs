cf42b2355cf99d00bdee714b08b3a2cdc553bf4d
Crunch (Chaz9) <99685295+CCIGAMES@users.noreply.github.com>
2023-03-03 17:44:44 +0000

    added some suggestions
    The script appears to be correct syntactically, but here are a few suggestions for best practices:
    
    Use proper indentation to improve code readability. The code inside each method should be indented to help distinguish blocks of code. This can be achieved by pressing the Tab key once for each level of indentation.
    
    Follow consistent naming conventions for variables and methods. This makes the code easier to read and understand. For example, use PascalCase for method names and camelCase for variable names.
    
    Use using statements for disposable objects. This will ensure that resources are properly released after use. For example, OpenRead method returns a stream that should be properly disposed of after use.

--- SonicHybridRsdk.Generator/RsdkSonicCdImporter.cs
+++ SonicHybridRsdk.Generator/RsdkSonicCdImporter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+
+using System.IO;
 using System.Linq;
 using static SonicHybridRsdk.Generator.Global;
 using static SonicHybridRsdk.Generator.RsdkGenericImporter;
@@ -7,14 +8,7 @@ namespace SonicHybridRsdk.Generator
 {
     class RsdkSonicCdImporter
     {
-        public static void UseStageV3(
-            Context context,
-            StageType stageType,
-            string name,
-            int actNumber,
-            string srcFolder,
-            string dstFolder,
-            string timeZone)
+        public static void UseStageV3(Context context, StageType stageType, string name, int actNumber, string srcFolder, string dstFolder, string timeZone)
         {
             var stages = context.SrcConfig.GetStages(stageType);
 
@@ -35,7 +29,7 @@ namespace SonicHybridRsdk.Generator
             File.Copy(Path.Combine(srcPath, "128x128Tiles.bin"), Path.Combine(dstPath, "128x128Tiles.bin"), true);
             File.Copy(Path.Combine(srcPath, "CollisionMasks.bin"), Path.Combine(dstPath, "CollisionMasks.bin"), true);
 
-            var background = OpenRead(Path.Combine(srcPath, "Backgrounds.bin"), StageBackgroundV3.Read);
+            using var background = OpenRead(Path.Combine(srcPath, "Backgrounds.bin"), StageBackgroundV3.Read);
             Create(Path.Combine(dstPath, "Backgrounds.bin"), stream => new StageBackgroundV4
             {
                 HLines = background.HLines,
@@ -52,14 +46,9 @@ namespace SonicHybridRsdk.Generator
                 }).ToList()
             }.Write(stream));
 
-            PatchStageConfig(context,
-                StageConfigV3.Read,
-                Path.Combine(srcPath, "StageConfig.bin"),
-                Path.Combine(dstPath, "StageConfig.bin"));
-            PatchStage(context,
-                StageActV3.Read,
-                Path.Combine(srcPath, $"Act{actNumber}.bin"),
-                Path.Combine(dstPath, $"Act{actNumber}.bin"),
+            PatchStageConfig(context, StageConfigV3.Read, Path.Combine(srcPath, "StageConfig.bin"), Path.Combine(dstPath, "StageConfig.bin"));
+
+            PatchStage(context, StageActV3.Read, Path.Combine(srcPath, $"Act{actNumber}.bin"), Path.Combine(dstPath, $"Act{actNumber}.bin"),
                 (context, entity, entityName) =>
                 {
                     switch (entityName)
@@ -73,36 +62,4 @@ namespace SonicHybridRsdk.Generator
                             entity.Y -= 20; // Fix Y placement
                             break;
                         case "Flower Pod":
-                            entity.Type = (byte)(context.DstObjects[entityName] + 1);
-                            entity.PropertyValue = 0;
-                            entity.Y += 16; // Fix Y placement
-                            break;
-                        default:
-                            return false;
-                    }
-
-                    return true;
-                },
-                (context, act) =>
-                {
-                    // Add title card
-                    act.Title = name.EndsWith(" ZONE") ? name[..^(" ZONE").Length] : name;
-                    var titleCardType = (byte)(context.DstObjects["Title Card"] + 1);
-                    var playerObject = act.Entities.FirstOrDefault();
-                    if (playerObject?.Type == 1 && !act.Entities.Any(x => x.Type == titleCardType))
-                    {
-                        act.Entities.Insert(2, new Entity
-                        {
-                            AttributeFlags = 0,
-                            Type = titleCardType,
-                            PropertyValue = (byte)(actNumber > 0 ? actNumber : 4),
-                            X = (short)(playerObject.X - 156),
-                            Y = (short)(playerObject.Y - 8),
-                        });
-                    }
-                });
-
-            context.DstConfig.GetStages(stageType).Add(dstStage);
-        }
-    }
-}
+                           
